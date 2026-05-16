371fa0ae97e39a44b052ac868538f62abe95d04c
ElhamFadel <elham2000fadel@gmail.com>
2026-05-16 16:15:03 +0300

    setup profiles and design schema

--- lib/db.ts
+++ lib/db.ts
@@ -1,11 +1,16 @@
-import { init } from '@instantdb/react';
+import { init } from '@instantdb/admin';
 
 const APP_ID = process.env.NEXT_PUBLIC_INSTANT_APP_ID!
+const token_db = process.env.INSTANT_ADMIN_TOKEN!
 
 if (!APP_ID) {
     throw new Error('Misding APP_ID enviroment variable')
 }
 
-const db = init({ appId: APP_ID })
+const db = init({
+    appId: APP_ID,
+    adminToken: token_db
+
+})
 
 export default db;
