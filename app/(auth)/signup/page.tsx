c5c84e3e3985ef25d384bdbdafdd326a9b4d227e
ElhamFadel <elham2000fadel@gmail.com>
2026-05-16 12:37:30 +0300

    added list of memebers

--- app/(auth)/signup/page.tsx
+++ app/(auth)/signup/page.tsx
@@ -1,3 +1,11 @@
+'use client'
+//temprory
+const COMMUNITY_EMAILS = [
+    'member1@test.com',
+    'member2@test.com',
+    'vip@tilted.com'
+]
 export default function SignupPage() {
+
     return <div>Signup Page</div>
 }
