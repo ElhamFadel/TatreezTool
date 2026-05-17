dba51ff9b9e4a36db21a7fc57a9e7918c46d334c
ElhamFadel <elham2000fadel@gmail.com>
2026-05-17 04:22:20 +0300

    fix: remove build-time check from admin.ts

--- lib/db.ts
+++ lib/db.ts
@@ -1,17 +1,10 @@
-import { init } from '@instantdb/admin';
-import _schema from './schema';
+import { init } from '@instantdb/admin'
+import _schema from './schema'
 
-const APP_ID = process.env.NEXT_PUBLIC_INSTANT_APP_ID ?? ''
-const token_db = process.env.INSTANT_ADMIN_TOKEN ?? ''
-
-if (!APP_ID) {
-    throw new Error('Misding APP_ID enviroment variable')
-}
-
-const db = init({
-    appId: APP_ID,
-    adminToken: token_db,
+const adminDb = init({
+    appId: process.env.NEXT_PUBLIC_INSTANT_APP_ID ?? '',
+    adminToken: process.env.INSTANT_ADMIN_TOKEN ?? '',
     schema: _schema,
 })
 
-export default db;
+export default adminDb
